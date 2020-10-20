5519b55ee8f34bd68ac59ab8aa6d6db87d0030b8
Uliana Utina <ulyana190909@yandex.ru>
2020-10-20 18:52:36 +0300

    выполнение первой части задания

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,42 @@
 package ru.netology.service;
 
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
 public class CashbackHackServiceTest {
+    @Test
+    public void CashbackTest1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 3500;
+        int actual = service.remain(amount);
+        int expected = 500;
+        assertEquals (actual, expected);
+    }
+
+    @Test
+    public void CashbackTest2() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+        assertEquals (actual, expected);
+    }
+
+    @Test
+    public void CashbackTest3() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals (actual, expected);
+    }
+    @Test
+    public void CashbackTest4() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1200;
+        int actual = service.remain(amount);
+        int expected = 800;
+        assertEquals (actual, expected);
+    }
 }
