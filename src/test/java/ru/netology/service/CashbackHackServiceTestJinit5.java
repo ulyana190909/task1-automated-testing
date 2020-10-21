50d04af4e108d93a4ff8f0c0f4648671f5df0681
Uliana Utina <ulyana190909@yandex.ru>
2020-10-21 11:56:55 +0300

    корректировка второго задания

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTestJinit5.java
@@ -0,0 +1,44 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class CashbackHackServiceTestJinit5 {
+
+    @Test
+    public void CashbackTest1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 3500;
+        int actual = service.remain(amount);
+        int expected = 500;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void CashbackTest2() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void CashbackTest3() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void CashbackTest4() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1200;
+        int actual = service.remain(amount);
+        int expected = 800;
+        assertEquals(expected, actual);
+    }
+}
